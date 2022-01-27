01244b0662f3882fb6e977e908f2673461f49fb2
Cedric <samcedric490@gmail.com>
2022-01-27 19:32:53 +0700

    Added new Error handling, instead returning a bunch of error log, the error message has been updated

--- hoyolab_daily_reward/__init__.py
+++ hoyolab_daily_reward/__init__.py
@@ -16,23 +16,32 @@ class GenshinClaim:
         asyncio.run(self.claim_reward(self.cookies))
 
     async def claim_reward(self, cookie: list) -> None:
-        for x in cookie:
-            self.client.set_cookies(x)
-            get_accounts = await self.client.genshin_accounts()
-            account = get_accounts[0].dict()
-            account_info = f'{account["uid"]} | {account["nickname"]} (AR {account["level"]})'
-            try:
-                reward = await self.client.claim_daily_reward()
-            except genshin.AlreadyClaimed:
-                print(f'Reward already claimed for: {account_info}')
-            else:
-                print(f'Claimed {reward.amount}x {reward.name} for {account_info}')
-        await self.client.close()
+        try:
+            for x in cookie:
+                self.client.set_cookies(x)
+                get_accounts = await self.client.genshin_accounts()
+                account = get_accounts[0].dict()
+                account_info = f'{account["uid"]} | {account["nickname"]} (AR {account["level"]})'
+                try:
+                    reward = await self.client.claim_daily_reward()
+                except genshin.AlreadyClaimed:
+                    print(f'Reward already claimed for: {account_info}')
+                else:
+                    print(f'Claimed {reward.amount}x {reward.name} for {account_info}')
+        except ValueError:
+            print(f'You must atleast add 1 account to claim daily reward.')
+        except genshin.InvalidCookies:
+            print('This cookies is invalid or already expired, please re-enter a new cookies.')
+        except Exception:
+            pass
+        finally:
+            await self.client.close()
 
     @staticmethod
     def get_account_list(accs: dict) -> list:
         with open(accs, 'r') as acc:
             data = json.load(acc)
         return data['accounts']
+
 
 genshin_claim = GenshinClaim
